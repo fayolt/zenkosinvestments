92add5e28a82e4d27cee9f7fb7f5d50702e5735f
Hippolyte-Fayol <fayolt@live.fr>
2016-04-19 21:33:59 +0800

    Adding more pages

---
+++ js/aside.js
@@ -0,0 +1,36 @@
+$("#team").owlCarousel({
+     
+          navigation : false, // Show next and prev buttons
+          slideSpeed : 300,
+          paginationSpeed : 400,
+          autoHeight : true,
+          itemsCustom : [
+                [0, 1],
+                [450, 2],
+                [600, 2],
+                [700, 2],
+                [1000, 4],
+                [1200, 4],
+                [1400, 4],
+                [1600, 4]
+              ],
+      });
+$("#clients").owlCarousel({
+  	 
+  	      navigation : false, // Show next and prev buttons
+  	      slideSpeed : 300,
+  	      paginationSpeed : 400,
+  	      autoHeight : true,
+  	      itemsCustom : [
+				        [0, 1],
+				        [450, 2],
+				        [600, 2],
+				        [700, 2],
+				        [1000, 4],
+				        [1200, 5],
+				        [1400, 5],
+				        [1600, 5]
+				      ],
+  	  });
+
+      
---
+++ js/main.js
@@ -0,0 +1,129 @@
+
+function main() {
+
+(function () {
+   'use strict';
+
+   /* ==============================================
+  	Testimonial Slider
+  	=============================================== */ 
+
+  	$('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 40
+            }, 900);
+            return false;
+          }
+        }
+      });
+
+    /*====================================
+    Preloader
+    ======================================*/
+	
+  	$(window).load(function() {
+
+   	// will first fade out the loading animation 
+    	$("#status").fadeOut("slow"); 
+
+    	// will fade out the whole DIV that covers the website. 
+    	$("#preloader").delay(500).fadeOut("slow").remove();      
+
+  	}); //;
+
+    /*====================================
+    accordian
+    ======================================*/
+    
+    $('.accordion-toggle').on('click', function(){
+      $(this).closest('.panel-group').children().each(function(){
+      $(this).find('>.panel-heading').removeClass('active');
+       });
+
+      $(this).closest('.panel-heading').toggleClass('active');
+    });
+
+    /*====================================
+    Show Menu on Book
+    ======================================*/
+    $(window).bind('scroll', function() {
+        var navHeight = $(window).height() - 100;
+        if ($(window).scrollTop() > navHeight) {
+            $('.navbar-default').addClass('on');
+        } else {
+            $('.navbar-default').removeClass('on');
+        }
+    });
+
+    $('body').scrollspy({ 
+        target: '.navbar-default',
+        offset: 80
+    }); //;
+
+  	$(document).ready(function() {
+  	  
+  	  $("#interest").owlCarousel({
+        navigation : false, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+        });
+
+      $("#team_branch").owlCarousel({
+        navigation : false, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+        });
+
+  	});
+
+  	/*====================================
+    Portfolio Isotope Filter
+    ======================================*/
+    $(window).load(function() {
+        var $container = $('.portfolio-items');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        $('.cat a').click(function() {
+            $('.cat .active').removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
+
+    });
+
+  	/*====================================
+    WOW JS
+    ======================================*/	
+
+	new WOW().init();
+	//smoothScroll
+	smoothScroll.init();
+
+
+	
+}());
+
+
+}
+main();
